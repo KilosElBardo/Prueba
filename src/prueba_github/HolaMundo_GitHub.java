89224b30c98ffe9aca045ef949a313301551a625
KilosElBardo <quilesebook@gmail.com>
2020-02-06 12:12:58 +0100

    pruba
    Signed-off-by: KilosElBardo <quilesebook@gmail.com>

---
+++ src/prueba_github/HolaMundo_GitHub.java
@@ -0,0 +1,11 @@
+package prueba_github;
+
+public class HolaMundo_GitHub {
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+
+		System.out.println("Hola Mundo");
+	}
+
+}
